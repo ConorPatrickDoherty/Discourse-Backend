0f3a22983b9cede3cfe81038c66f74c1b67ca724
ConorDoherty1998 <Conordoherty980@gmail.com>
2020-04-22 13:43:52 +0100

    user can't create an account with a pre-existing username

--- functions/src/collections/users.ts
+++ functions/src/collections/users.ts
@@ -5,39 +5,43 @@ import { User } from '../interfaces/user';
 import { UploadImage } from './storage'
 import * as Queries from './shared-queries'
 
-const db = admin.firestore().collection('Users')
+const Users = admin.firestore().collection('Users')
 
 export const CreateUser = functions.region(Region).https.onCall((body, event) => {
     if (!body.credentials) throw new functions.https.HttpsError('invalid-argument', 'Invalid Credentials')
 
-    return admin.auth().createUser({
-        email: body.credentials.email.toLowerCase(),
-        displayName: body.credentials.username,
-        password: body.credentials.password
-    }).then((x) => {
-        const user: User = {
+    return Users.where('username', '==', body.credentials.username).get().then((u) => {
+        if (u.docs.length) throw new functions.https.HttpsError('invalid-argument', 'The username is already in use by another account')
+    }).then(() => {
+        return admin.auth().createUser({
             email: body.credentials.email.toLowerCase(),
-            username: body.credentials.username,
-            createdAt: admin.firestore.FieldValue.serverTimestamp(),
-            score: 0,
-            role: 'User'
-        }
-
-        return db.doc(x.uid).set(user).then((doc) => {
-            return user
+            displayName: body.credentials.username,
+            password: body.credentials.password
+        }).then((x) => {
+            const user: User = {
+                email: body.credentials.email.toLowerCase(),
+                username: body.credentials.username,
+                createdAt: admin.firestore.FieldValue.serverTimestamp(),
+                score: 0,
+                role: 'User'
+            }
+            return Users.doc(x.uid).set(user).then((doc) => {
+                return user
+            }).catch((error) => {
+                throw new functions.https.HttpsError('invalid-argument', error.message)
+            })
         }).catch((error) => {
-            throw new functions.https.HttpsError('invalid-argument', error)
+            throw new functions.https.HttpsError('invalid-argument', error.message)
         })
-    }).catch((error) => {
-        throw new functions.https.HttpsError('invalid-argument', error)
     })
+    
 });
 
 export const GetUser = functions.region(Region).https.onCall((body, event) => {
     if (!event.auth) throw new functions.https.HttpsError('permission-denied', 'Not signed in')
     if (!body.email) throw new functions.https.HttpsError('invalid-argument', 'Invalid Email')
 
-    return db.where('email', '==', body.email.toLowerCase()).get().then((doc) => {
+    return Users.where('email', '==', body.email.toLowerCase()).get().then((doc) => {
         console.log(doc.docs[0].data())
         return doc.docs[0].data() as User
     })
